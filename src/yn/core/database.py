171726d4b924c38de93e50fdb786bd326d45dc10
psk <shaban.benito.dimitri@gmail.com>
2026-05-05 18:04:58 +0300

    feat: configuring minimal project

---
+++ src/yn/core/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from yn.core.settings import settings
+
+
+async_engine = create_async_engine(settings.database_url, echo=True)
+async_session = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
+
+class Base(DeclarativeBase):   
+    pass
---
+++ src/yn/main.py
@@ -0,0 +1,38 @@
+from typing import AsyncGenerator
+
+import uvicorn
+
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from yn.core.settings import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    print("Starting up...")
+    yield
+    print("Shutting down...")
+
+
+app = FastAPI(
+    title="Yukinoise API",
+    description="API for Yukinoise, a music higload service built with FastAPI and Python for independent artists",
+    version="0.0.1",
+    lifespan=lifespan,
+)
+
+
+@app.get("/")
+async def health_check() -> dict[str, str]:
+    return {"message": "health check passed"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host=settings.app_host,
+        port=settings.app_port,
+        workers=settings.app_workers,
+    )
